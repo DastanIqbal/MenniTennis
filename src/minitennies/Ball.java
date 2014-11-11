27d70bef8b6b99f922ddcd3cbf9136d38fdae9d5
Sinbad Iqbal <ask2iqbal@gmail.com>
2014-11-11 17:12:34 +0530

    Move code to minitimes package

---
+++ src/minitennies/Ball.java
@@ -0,0 +1,125 @@
+package minitennies;
+import java.awt.Rectangle;
+import java.util.Random;
+
+public class Ball {
+
+	private int SPEEDX = 3;
+	private int SPEEDY = 3;
+	private int RANDOMX = 15;
+	private int RANDOMY = 10;
+
+	private int x, y, dx, dy, speedX = SPEEDX, speedY = SPEEDY;
+	boolean iscollideX = false;
+	boolean iscollideY = false;
+	private Random r;
+	public Rectangle rect;
+	Main main;
+	Brick brick;
+	public boolean isgameover=false;
+
+	public Ball(int x, int y, Main main, Brick brick) {
+		this.x = x;
+		this.y = y;
+		this.main = main;
+		rect = new Rectangle();
+		r = new Random();
+		this.brick = brick;
+	}
+
+	/**
+	 * @return the sPEEDX
+	 */
+	public int getSPEEDX() {
+		return SPEEDX;
+	}
+
+	/**
+	 * @return the sPEEDY
+	 */
+	public int getSPEEDY() {
+		return SPEEDY;
+	}
+
+	/**
+	 * @param sPEEDX
+	 *            the sPEEDX to set
+	 */
+	public void setSPEEDX(int sPEEDX) {
+		SPEEDX = sPEEDX;
+	}
+
+	/**
+	 * @param sPEEDY
+	 *            the sPEEDY to set
+	 */
+	public void setSPEEDY(int sPEEDY) {
+		SPEEDY = sPEEDY;
+	}
+
+	public void move() {
+		
+		x += speedX;
+		y += speedY;
+		
+		if (x < 0) {
+			speedX = SPEEDX;
+		}
+
+		if (x > main.getSize().width) {
+			speedX = -SPEEDX;
+		}
+
+		if (y < 0) {
+			speedY = SPEEDY;
+		}
+		if (y > main.getSize().height) {
+			//speedY = -SPEEDY;
+			isgameover=true;
+		}
+
+		detectCollision();
+
+		rect.setBounds(x - 10, y - 10, 20, 20);
+	}
+
+	private void detectCollision() {
+
+		if (x >= brick.getX() && x <= brick.getX() + 100) {
+			if (y >= brick.getY()-10) {
+				speedY = -SPEEDY;
+			}
+		}
+	}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * @param x
+	 *            the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * @param y
+	 *            the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
---
+++ src/minitennies/Main.java
@@ -0,0 +1,113 @@
+package minitennies;
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Main extends Applet implements Runnable, KeyListener {
+
+	private Image image;
+	private Graphics second;
+	public Ball ball;
+	private Brick brick;
+
+	@Override
+	public void init() {
+
+		setSize(400, 400);
+		setBackground(Color.WHITE);
+		setFocusable(true);
+
+		addKeyListener(this);
+
+	}
+
+	@Override
+	public void start() {
+		brick = new Brick(0, 370, this);
+		ball = new Ball(0, 0, this, brick);
+		Thread t = new Thread(this);
+		t.start();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		if (!ball.isgameover) {
+			g.setColor(Color.RED);
+			g.fillOval(ball.getX(), ball.getY(), 20, 20);
+			g.setColor(Color.BLUE);
+			g.fillRect(brick.getX(), brick.getY(), 100, 10);
+		} else {
+			setBackground(Color.BLACK);
+			g.setColor(Color.WHITE);
+			g.drawString("Game Over", (getWidth()-30) / 2, getHeight() / 2);
+		}
+	}
+
+	@Override
+	public void update(Graphics g) {
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+
+		second.setColor(getBackground());
+		second.fillRect(0, 0, this.getWidth(), this.getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+	}
+
+	@Override
+	public void stop() {
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void run() {
+
+		while (true) {
+			ball.move();
+			repaint();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_LEFT:
+			brick.moveLeft();
+			break;
+		case KeyEvent.VK_RIGHT:
+			brick.moveRight();
+			break;
+
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+}
